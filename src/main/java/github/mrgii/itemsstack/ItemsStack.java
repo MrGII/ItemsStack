9ebc2318dcf31842b58fc6c7af277efef4e4e6e1
MrGII <gilad.zohar.shay@gmail.com>
2024-07-29 11:00:11 +0300

    Removed useless tag log in onInitialize, this will not get pushed to release since it does not affect any gameplay.

--- src/main/java/github/mrgii/itemsstack/ItemsStack.java
+++ src/main/java/github/mrgii/itemsstack/ItemsStack.java
@@ -5,7 +5,6 @@ import github.mrgii.itemsstack.util.ItemsStackSizeModifier;
 import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
-import net.fabricmc.fabric.api.tag.convention.v2.ConventionalItemTags;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,12 +14,9 @@ public class ItemsStack implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		String tagName = ConventionalItemTags.TOOLS.getName().getString();
-		LOGGER.info(tagName);
 		ServerLifecycleEvents.SERVER_STARTED.register((minecraftServer) -> {
 			ItemsStackSizeModifier.updateValuesAndModifyStackSizes();
 			ItemsStackSizeModifier.registerCallbacks();
 		});
-
 	}
 }
